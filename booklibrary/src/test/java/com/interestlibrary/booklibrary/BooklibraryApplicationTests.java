29f98cd929872568e644411101e5365d02a6f0ad
MLysiak2 <Maciej.Lysiak@t-mobile.pl>
2022-08-03 14:59:40 +0200

    Start of a new project

---
+++ booklibrary/src/test/java/com/interestlibrary/booklibrary/BooklibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interestlibrary.booklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
